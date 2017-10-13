11543eb9f5695c689eb75e2f65ad6c2a7753134f
StoneInc <git@sobadev.com>
2017-10-13 13:29:43 +0200

    Working n-user MEETPOINT-Algorithm

--- ugm_algorithms.py
+++ ugm_algorithms.py
@@ -51,52 +51,42 @@ def dijkstra(data, P1, P2):
 # Find the point best to meet at for people at P1 and P2
 # where data is the relationship matrix (numpy matrix) and 
 # P1/P2 are the indices of the stations in the matrix (From Column to Row)
-def findMeetPoint(data, P1, P2):
+def findMeetPoint(data, positions):
     #Suffix _P: P1 -> P2
     #Suffix _M: P2 -> P1
     data = np.array(data);
-    vertices_P = np.array(range(0, data.shape[0]));
-    vertices_M = np.array(range(0, data.shape[0]));
-    knownPoints_P = [];
-    knownPoints_M = [];
-    dist_P = {};
-    prev_P = {};
-    dist_M = {};
-    prev_M = {};
+    vertices = [np.array(range(0, data.shape[0]))] * len(positions);
+    knownPoints = [[]] * len(positions);
+    prev = np.zeros( (len(positions), data.shape[0]) );
+    dist = np.zeros( (len(positions), data.shape[0]) );
 
+    for psn in range(0, len(positions)):
+        for vertex in vertices[psn]:
+            dist[psn][vertex] = INFINITY;
+            prev[psn][vertex] = UNDEFINED;
+        dist[psn][positions[psn]] = 0;
 
-    for vertex in vertices_P:
-        dist_P[vertex] = INFINITY;
-        prev_P[vertex] = UNDEFINED;
-    for vertex in vertices_M:
-        dist_M[vertex] = INFINITY;
-        prev_M[vertex] = UNDEFINED;
+    while (True): #The algorithm will terminate eventually
+        current_vertex = [0] * len(positions);
+        for psn in range(0, len(positions)):
+            current_vertex[psn] = min(vertices[psn], key=lambda x: dist[psn][x]) #closest neighbor
+            vertices[psn] = [] if len(vertices[psn])==1 else vertices[psn][vertices[psn]!=current_vertex[psn]]; #remove the node from the unknown set "Q"
+            knownPoints[psn] = np.append(knownPoints[psn], current_vertex[psn]); #add node to known set
 
-    dist_P[P1] = 0;
-    dist_M[P2] = 0;
+            #Now iterate over all connections departing current_vertex except itself (remove -1 connections)
+            for neighbor in [key for key in range(0, data.shape[0]) 
+                                 if key!=current_vertex[psn] and
+                                    data[:, 
+                                    current_vertex[psn]
+                                    ]
+                                    [key]!=-1]:
+                alt = dist[psn][current_vertex[psn]] + data[current_vertex[psn], neighbor];
+                if (alt < dist[psn][neighbor]): #If new route is better, change it
+                    dist[psn][neighbor] = alt;
+                    prev[psn][neighbor] = current_vertex[psn];
 
-    while (len(vertices_P) > 0 and len(vertices_M) > 0): #while there are nodes left in the unknown set "Q"
-        current_vertex_P = min(vertices_P, key=lambda x: dist_P[x]) #closest neighbor
-        current_vertex_M = min(vertices_M, key=lambda x: dist_M[x])
-        vertices_P = [] if len(vertices_P)==1 else vertices_P[vertices_P!=current_vertex_P]; #remove the node from the unknown set "Q"
-        vertices_M = [] if len(vertices_M)==1 else vertices_M[vertices_M!=current_vertex_M];
-        knownPoints_P = np.append(knownPoints_P, current_vertex_P);
-        knownPoints_M = np.append(knownPoints_M, current_vertex_M);
-
-        #Now iterate over all connections departing current_vertex except itself (remove -1 connections)
-        for neighbor in [key for key in range(0, data.shape[0]) if key!=current_vertex_P and data[:, current_vertex_P][key]!=-1]:
-            alt = dist_P[current_vertex_P] + data[current_vertex_P, neighbor];
-            if (alt < dist_P[neighbor]): #If new route is better, change it
-                dist_P[neighbor] = alt;
-                prev_P[neighbor] = current_vertex_P;
-
-        for neighbor in [key for key in range(0, data.shape[0]) if key!=current_vertex_M and data[:, current_vertex_M][key]!=-1]:
-            alt = dist_M[current_vertex_M] + data[current_vertex_M, neighbor];
-            if (alt < dist_M[neighbor]): #If new route is better, change it
-                dist_M[neighbor] = alt;
-                prev_M[neighbor] = current_vertex_M;
-
-        knownIntersection = np.intersect1d(knownPoints_P, knownPoints_M);
+        from functools import reduce
+        knownIntersection = reduce(np.intersect1d, knownPoints)
         if (len(knownIntersection) > 0): return knownIntersection;
 
     return -1;
@@ -107,4 +97,4 @@ test_graph = [[ 0,  1, -1,  8, -1], #Fastest path [AE] is A-B-C-D-E [WANTED]
               [ 8, -1,  1,  0,  1],
               [-1, -1,  4,  1,  0]]
 
-print(findMeetPoint(test_graph, 0, 4)) #Find fastest path for [AE] and print it
+print(findMeetPoint(test_graph, [0, 1, 0])) #Find fastest path for [AE] and print it
