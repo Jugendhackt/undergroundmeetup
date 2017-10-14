a3303bec950bdfbd67687221209636ac73271579
StoneInc <git@sobadev.com>
2017-10-14 06:23:06 +0200

    Propably working code; Multiple bug fixes

--- ugm_interface.py
+++ ugm_interface.py
@@ -3,10 +3,11 @@ import csv
 from ugm_algorithms import findMeetPoint
 
 header = open('data.csv').readline().split(";")
-data = np.loadtxt(open("data.csv", "rb"), delimiter=";", skiprows=1)
+data = np.loadtxt(open("data.csv", "rb"), delimiter=";", skiprows=1).transpose(1, 0)
+print(data[1][0])
 print(data.shape)
 print(len(header))
-locations = [32, 17]
+locations = [14, 18]
 print("Finding meeting point for:")
 for station in locations:
     print(" - " + str(header[station]))
