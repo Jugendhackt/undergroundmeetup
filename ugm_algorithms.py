074527033332b155706fbcb16224f96e72ac5957
StoneInc <git@sobadev.com>
2017-10-14 04:50:52 +0200

    Maybe bugfree bulletproof 'MEETPOINT'

--- ugm_algorithms.py
+++ ugm_algorithms.py
@@ -29,7 +29,7 @@ def dijkstra(data, P1, P2):
 
     dist[P1] = 0;
 
-    while (len(vertices) > 0): #while there are nodes left in the unknown set "Q"
+    while (len(vertices)): #while there are nodes left in the unknown set "Q"
         current_vertex = min(vertices, key=lambda x: dist[vertex]) #closest neighbor
         vertices = [] if len(vertices)==1 else vertices[vertices!=current_vertex]; #remove the node from the unknown set "Q"
         print(current_vertex);
@@ -40,7 +40,7 @@ def dijkstra(data, P1, P2):
                 dist[neighbor] = alt;
                 prev[neighbor] = current_vertex;
 
-    return prev;
+    return prev
 
 
 
@@ -66,32 +66,35 @@ def findMeetPoint(data, positions):
             prev[psn][vertex] = UNDEFINED;
         dist[psn][positions[psn]] = 0;
 
-    while (True): #The algorithm will terminate eventually
-        current_vertex = [0] * len(positions);
+    while (len(min(vertices, key=lambda x: len(x))) > 0): #The algorithm will terminate eventually
         for psn in range(0, len(positions)):
-            current_vertex[psn] = min(vertices[psn], key=lambda x: dist[psn][x]) #closest neighbor
-            vertices[psn] = [] if len(vertices[psn])==1 else vertices[psn][vertices[psn]!=current_vertex[psn]]; #remove the node from the unknown set "Q"
-            knownPoints[psn] = np.append(knownPoints[psn], current_vertex[psn]); #add node to known set
+            #NOT WORKING
+            min_dist = min([v for k,v in np.ndenumerate(dist[psn]) if k in vertices[psn]]);
+            current_vertices = [v for (k,v) in np.ndenumerate(vertices[psn]) if dist[psn][v] == min_dist];
+            #current_vertices <- all vertices for [psn] with min dist[psn][i]
+            for current_vertex in current_vertices:
+                vertices[psn] = [] if len(vertices[psn])==1 else vertices[psn][vertices[psn]!=current_vertex]; #remove the node from the unknown set "Q"
+                knownPoints[psn] = np.append(knownPoints[psn], current_vertex); #add node to known set
 
-            #Now iterate over all connections departing current_vertex except itself (remove -1 connections)
-            for neighbor in [key for key in range(0, data.shape[0]) 
-                                 if key!=current_vertex[psn] and data[:, current_vertex[psn]][key]!=-1]:
-                alt = dist[psn][current_vertex[psn]] + data[current_vertex[psn], neighbor];
-                if (alt < dist[psn][neighbor]): #If new route is better, change it
-                    dist[psn][neighbor] = alt;
-                    prev[psn][neighbor] = current_vertex[psn];
+                #Now iterate over all connections departing current_vertex except itself (remove -1 connections)
+                neighbors = [key for key in range(0, data.shape[0]) 
+                                    if key!=current_vertex and data[:, current_vertex][key]!=-1]
+                for neighbor in neighbors:
+                    alt = dist[psn][current_vertex] + data[current_vertex, neighbor];
+                    if (alt < dist[psn][neighbor]): #If new route is better, change it
+                        dist[psn][neighbor] = alt;
+                        prev[psn][neighbor] = current_vertex;
 
         from functools import reduce
         knownIntersection = reduce(np.intersect1d, knownPoints)
         if (len(knownIntersection) == 1): return knownIntersection[0];
         if (len(knownIntersection) > 1): return int(min(knownIntersection, key=lambda x: max(dist[:,int(x)])));
-
     return -1;
             #   A   B   C   D   E (FROM)
-test_graph = [[ 0,  1, -1,  8, -1], #Fastest path [AE] is A-B-C-D-E [WANTED]
-              [ 1,  0,  2, -1, -1], #Shortest path [AE] is A-D-E
-              [-1,  2,  0,  1,  4],
-              [ 8, -1,  1,  0,  1],
-              [-1, -1,  4,  1,  0]]
-
-print(findMeetPoint(test_graph, [0, 2, 0])) #Find fastest path for [AE] and print it
+#test_graph = [[ 0,  1, -1,  8, -1], #Fastest path [AE] is A-B-C-D-E [WANTED]
+#              [ 1,  0,  2, -1, -1], #Shortest path [AE] is A-D-E
+#              [-1,  2,  0,  1,  4],
+#              [ 8, -1,  1,  0,  1],
+#              [-1, -1,  4,  1,  0]]
+#
+#print(findMeetPoint(test_graph, [0, 2, 0])) #Find fastest path for [AE] and print it
