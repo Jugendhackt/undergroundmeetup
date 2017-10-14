12487d2056042f3b42cba9d9b93d10d97fb4e471
StoneInc <levyehrstein@googlemail.com>
2017-10-14 04:50:52 +0200

    Maybe bugfree bulletproof 'MEETPOINT'

--- ugm_interface.py
+++ ugm_interface.py
@@ -1,5 +1,13 @@
-from numpy import genfromtxt
+import numpy as np
+import csv
+from ugm_algorithms import findMeetPoint
 
-data = genfromtxt('data.csv', delimiter=',')
+header = open('data.csv').readline().split(";")
+data = np.loadtxt(open("data.csv", "rb"), delimiter=";", skiprows=1)
 print(data.shape)
-print(data)
+print(len(header))
+locations = [32, 17]
+print("Finding meeting point for:")
+for station in locations:
+    print(" - " + str(header[station]))
+print("=> Meeting point: " + str(header[int(findMeetPoint(data, locations))]))
