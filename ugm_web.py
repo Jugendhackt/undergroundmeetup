f294d18ec5980d2875e74864aa9b1025fef87079
sternenseemann <git@lukasepple.de>
2017-10-14 04:44:31 +0200

    Further web preparations

--- ugm_web.py
+++ ugm_web.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, json, make_response
 app = Flask(__name__)
+api_prefix = "/api/v1"
 
 @app.route("/")
 def index():
-    return render_template("index.html", stylesheet=url_for('static', filename='main.css'))
+    return render_template("index.html",
+            stylesheet=url_for('static', filename='main.css'),
+            script=url_for('static', filename='main.js'))
+
+@app.route(api_prefix + "/meetup", methods=['POST'])
+def meetup():
+    data = request.get_json()
+    resp = make_response(json.jsonify(meetup="G04"))
+    resp.mimetype = 'application/json'
+    return resp
